a9e4b458d8a2ccd3acd77cfc9c9f7eb0ce15af44
Ho Yin <hello@leunghoyin.hk>
2022-01-07 00:04:33 +0000

    useEffect Hook Notes

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,21 +1,35 @@
 import {Link} from "react-router-dom"
 import styles from './ProductList.module.css'
-import {useState} from "react"  //React Hook
+import {useState, useEffect} from "react"  //React Hook
 import Title from "./Title"
 
 export default function ProductList() {
 
     let [productList, setProductList] = useState([])
-    
-    console.log(productList)
+    let [input , setInput] = useState('')
 
-    fetch('https://hoyinleung.github.io/demoapi/react-basic-product.json')
-        .then(response => response.json())
-        .then(data => setProductList(data))
+    //useEffect
+    useEffect(()=>{
+        //1 : 無第二個參數 : component每次render都會觸發
+        //2 : Dependency Array是空array時 : 只會在第一次網頁render時會觸發
+        //3 : Dependency Array是有變數時 : 第一次網頁render時 + 指定的變數改變 會觸發
+        fetch('https://hoyinleung.github.io/demoapi/react-basic-product.json')
+            .then(response => response.json())
+            .then(data => setProductList(data))
+            
+        console.log(productList)
+    },[]) // <==  Dependency Array
+
+    useEffect(()=>{
+        if(input.length>4)
+            console.log('字串夠長')
+        else
+            console.log('字串太短')
+    },[input])
 
     return (
         <div>
-
+            <input type="text" onChange={e=>setInput(e.target.value)} />
             <Title mainTitle="請選擇要購買的水果" />
             
             <div>
