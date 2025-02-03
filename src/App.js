d394d28b5448b993e5fb3248ca6167c95a05951a
Philip Leung <leunghoyinhk@gmail.com>
2025-02-03 18:40:09 +0000

    add link

--- src/App.js
+++ src/App.js
@@ -2,38 +2,40 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Checkout from './Checkout';
 import ProductDetail from './ProductDetail';
 import ProductList from './ProductList';
-import {CartContext} from './CartContext'
+import { CartContext } from './CartContext'
 import { useState } from 'react';
 
 function App() {
 
 	const [cartItems, setCartItems] = useState([])
 
-  return (
-    <BrowserRouter>
+	return (
+		<BrowserRouter>
 
-		<CartContext.Provider value={{cartItems,setCartItems}}>
+			<CartContext.Provider value={{ cartItems, setCartItems }}>
 
-			<nav>
-				<Link to="/">首頁</Link> 
-				<Link to="/checkout">購物車</Link>
-			</nav>
+				<nav>
+					<Link to="/">首頁</Link>
+					<Link to="/checkout">購物車</Link>
+				</nav>
 
-			<Routes>
-				<Route path="/" element={<ProductList/>} />
-				<Route path="/checkout" element={<Checkout/>} />
+				<Routes>
+					<Route path="/" element={<ProductList />} />
+					<Route path="/checkout" element={<Checkout />} />
 
-				<Route path="/product" element={<ProductDetail/>}>
-					<Route path=":id" element={<ProductDetail/>} />
-				</Route>
+					<Route path="/product" element={<ProductDetail />}>
+						<Route path=":id" element={<ProductDetail />} />
+					</Route>
 
-				<Route path="*" element={<p>找不到頁面</p>}/>
-			</Routes>
+					<Route path="*" element={<p>找不到頁面</p>} />
+				</Routes>
+				<footer style={{ textAlign: "center" }}>
+					By <a href="https://www.leunghoyin.hk" target='_blank'>梁浩賢</a>
+				</footer>
+			</CartContext.Provider>
 
-		</CartContext.Provider>
-
-	</BrowserRouter>
-  );
+		</BrowserRouter>
+	);
 }
 
 export default App;
